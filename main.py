be67de1e5006914fad5449db4df3a19c7796c4a3
Juciandro Souza <juciandro59@gmail.com>
2023-10-28 11:06:14 -0400

    novas alterações

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "Git Exercise"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, query_param: str = None):
+    return {"item_id": item_id, "query_param": query_param}
